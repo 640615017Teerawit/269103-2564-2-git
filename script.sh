82bf09b71d71b63caf89fd9b23524c0eb7c74f89
640615017Teerawit <93969715+640615017Teerawit@users.noreply.github.com>
2022-01-08 15:28:03 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+#Make OUTPUT a file named backup_ followed by the date %Y%m%d and make this file type .tgz .
 OUTPUT=~/backup_$(date +%Y%m%d).tgz
+#unarchive file  OUTPUT to the directory
 tar -cvzf $OUTPUT $1
